0490f4c18f69d751dfa8e85ab5bc56bd75a8bfa8
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-03-18 05:41:06 +0900

    Update fms.c

--- src/fms.c
+++ src/fms.c
@@ -8,9 +8,22 @@ int RC4(int *IV, int *key) {
     int K[256];
     int S[256];
     int seed[16];
-    int i, j, l, t;
+    int i, j, k, t;
 
     //시드 = IV + Key
 
-    
+    for(k=0; k<3; k++) 
+
+        seed[k] = IV[k];
+
+    for(k=0; k<13; k++) 
+
+        seed[k + 3] = key[k];
+
+// -= 키 스케줄링 알고리즘 (KSA) =-
+
+// 배열 초기화
+
+for
+
 }
