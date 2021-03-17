fd73fc3b1e190b033a551c4ec180b8d12cb6ec28
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-03-18 05:34:59 +0900

    Update fms.c

--- src/fms.c
+++ src/fms.c
@@ -1,1 +1,16 @@
+//Kinesys FMS Attack 
+/* FMS Attack : RC4 키 스케줄링 알고리즘의 약점을 이용한 WEP공격*/
 
+#include <stdio.h>
+
+/* RC4 스트링 암호 */
+int RC4(int *IV, int *key) {
+    int K[256];
+    int S[256];
+    int seed[16];
+    int i, j, l, t;
+
+    //시드 = IV + Key
+
+    
+}
